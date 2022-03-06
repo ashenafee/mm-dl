834b288ded5eaeeef67ee9784f73972d6bba743c
ashenafee <ashenafeem@gmail.com>
2022-03-06 13:11:55 -0500

    Added local install for ffmpeg

--- mm-dl.py
+++ mm-dl.py
@@ -9,9 +9,12 @@ if (len(sys.argv) != 3):
     print("Usage: python3 mm-dl.py <MY_MEDIA_LINK> <FILENAME>")
     exit(1)
 
+# Flag for which ffmpeg to use
+flag = False
+
 # Check if ffmpeg is in path
-if (which("ffmpeg") == None):
-    print("ffmpeg not found in PATH.\nPlease install ffmpeg from https://ffmpeg.org.")
+if (which("ffmpeg") == None and (os.path.isdir("ffmpeg-5.0-essentials_build") == False or os.path.isfile("ffmpeg") == False)):
+    print("ffmpeg not found in PATH.")
 
     # Ask whether to install ffmpeg locally
     print("\nDo you want to install ffmpeg locally? (y/n)")
@@ -20,14 +23,23 @@ if (which("ffmpeg") == None):
         
         # Check if macOS or Windows
         if (sys.platform == "darwin"):
-            r = requests.get("https://evermeet.cx/ffmpeg/ffmpeg-5.0.7z")
+            r = requests.get("https://evermeet.cx/ffmpeg/ffmpeg-5.0.zip")
+            open("ffmpeg-5.0.zip", "wb").write(r.content)
+            print("Extracting ffmpeg...")
+            os.system("unzip ffmpeg-5.0.zip")
         elif (sys.platform == "win32"):
             r = requests.get("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip", allow_redirects=True)
+            open("ffmpeg-release-essentials.zip", "wb").write(r.content)
+            print("Extracting ffmpeg...")
+            
+            # Current directory
+            cd = os.getcwd()
+            os.system(f"Expand-Archive -Force {cd}\ffmpeg-release-essentials.zip {cd}\ffmpeg-release-essentials")
         
-        open("ffmpeg-release-essentials.zip", "wb").write(r.content)
-        print("Extracting ffmpeg...")
-        os.system("unzip ffmpeg-release-essentials.zip")
-    exit(1)
+        flag = True
+    else:
+        print("Exiting...")
+        exit(1)
 
 # Check if ffmpeg-bar is installed
 if (which("ffmpeg-bar") == None):
@@ -40,5 +52,19 @@ ID=sys.argv[1][-32:]
 print(f"Downloading MyMedia ID: '{ID}'")
 
 # Run FFMPEG command in terminal
-os.system(f"ffmpeg-bar -i \"https://stream.library.utoronto.ca:1935/MyMedia/play/mp4:1/{ID}.mp4/playlist.m3u8\" -codec copy \"{sys.argv[2]}.mkv\"")
-print(f"Success: MyMedia video '{ID}' has been downloaded!")
+if (flag):
+    # Use normal ffmpeg
+    os.system(f"./ffmpeg-5.0-essentials_build/bin/ffmpeg.exe -i \"https://stream.library.utoronto.ca:1935/MyMedia/play/mp4:1/{ID}.mp4/playlist.m3u8\" -codec copy \"{sys.argv[2]}.mkv\"")
+
+    if sys.platform == "win32":
+        ffmpeg_instructions = "https://www.wikihow.com/Install-FFmpeg-on-Windows"
+    elif sys.platform == "darwin":
+        ffmpeg_instructions = "https://superuser.com/questions/624561/install-ffmpeg-on-os-x"
+
+    print(f"Success: MyMedia video '{ID}' has been downloaded!")
+    print(f"If you would like to see a neat progress bar when downloading, please install FFmpeg to your PATH. \
+            More information can be found here: {ffmpeg_instructions}.")
+else:
+    # Use ffmpeg-bar
+    os.system(f"ffmpeg -i \"https://stream.library.utoronto.ca:1935/MyMedia/play/mp4:1/{ID}.mp4/playlist.m3u8\" -codec copy \"{sys.argv[2]}.mkv\"")
+    print(f"Success: MyMedia video '{ID}' has been downloaded!")
